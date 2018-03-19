45154220ca5ec8d8a90b20a91f4d693ddc41a011
Your Namekrishna <krishnarcmurali@gmail.com>
2018-03-19 17:19:59 +0530

    'second'

---
+++ simplesocial/simplesocial/views.py
@@ -0,0 +1,11 @@
+from django.views.generic import TemplateView
+
+# Create your views here.
+class TestPage(TemplateView):
+    template_name='test.html'
+
+class ThanksPage(TemplateView):
+    template_name='thanks.html'
+
+class HomePage(TemplateView):
+    template_name='index.html'
